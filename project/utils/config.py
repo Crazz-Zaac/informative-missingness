e49b5d8c7f40e31f849301ae6d3cb67eeecef84a
crazz-zaac <r.alpha919@gmail.com>
2025-05-30 00:15:23 +0200

    db credentials updated

--- project/utils/config.py
+++ project/utils/config.py
@@ -1,19 +1,21 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing import Optional
+from pathlib import Path
 
 class DatabaseSettings(BaseSettings):
-    DB_USER: str = "postgres"       # Your PostgreSQL username
-    DB_PASS: str = "postgres"       # Your PostgreSQL password
-    DB_HOST: str = "localhost"      # Since you mapped 5432:5432
-    DB_PORT: str = "5432"           # Default PostgreSQL port
-    DB_NAME: str = "mimiciv"        
-    
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    # .env file is located in the project root directory
+    _env_file_path = Path(__file__).parent.parent / ".env"
     model_config = SettingsConfigDict(
-        env_file = ".env",
-        env_file_encoding = "utf-8",
-        env_nested_delimiter = "__"
+        env_file=_env_file_path,
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__"
     )
-    
+
     def get_db_uri(self) -> str:
-        url =  f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        url = f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         return url
